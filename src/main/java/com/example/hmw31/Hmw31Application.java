afc22c4c8413b7a2f15daeb44008c9ec02a47337
garry <garry4978@gmail.com>
2023-03-01 09:52:12 +0300

    Урок 6. Spring Boot

---
+++ src/main/java/com/example/hmw31/Hmw31Application.java
@@ -0,0 +1,13 @@
+package com.example.hmw31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hmw31Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hmw31Application.class, args);
+    }
+
+}
